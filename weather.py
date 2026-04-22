4a2d1d7b42a9c27ddd46f839545c62c0acf47675
Aryan Deepak <aryan-mac@Aryans-MacBook-Air-2.local>
2026-04-22 09:52:07 +0530

    final weather app

--- weather.py
+++ weather.py
@@ -1,20 +1,20 @@
 import requests
 
-API_KEY = "4216c03f4559fd119c25168dfddded46"
+API_KEY = "a63c0ea3b8bf482fbf441845262204"
 
 def get_weather(city):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-    
+    url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}"
+
     response = requests.get(url)
     data = response.json()
 
-    if data["cod"] != 200:
+    if "error" in data:
         print("❌ City not found")
         return
 
-    temp = data["main"]["temp"]
-    humidity = data["main"]["humidity"]
-    condition = data["weather"][0]["description"]
+    temp = data["current"]["temp_c"]
+    humidity = data["current"]["humidity"]
+    condition = data["current"]["condition"]["text"]
 
     print(f"\n🌍 City: {city}")
     print(f"🌡️ Temperature: {temp}°C")
